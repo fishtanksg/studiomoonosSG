f33c24f25f180deafbf53f5cc3608d17d5d209b7
fishinde <fish@onesignal.com>
2025-01-17 19:36:29 +0800

    Update OneSignalMethods.js

--- js/onesignal/OneSignalMethods.js
+++ js/onesignal/OneSignalMethods.js
@@ -12,7 +12,7 @@
         
         allowLocalhostAsSecureOrigin: true,
         appId: "3c3d6636-8648-4994-8c79-92da1cc22fa0", //main
-        serviceWorkerParam: { scope: "/studiomoonos fishtank/" },
+        serviceWorkerParam: { scope: "/studiomoonosSG/" },
         //serviceWorkerParam: { scope: "/studiomoonos/" },
         path: "OneSignalSDKWorker.js", // this always set to OneSignalSDKWorker.js by server config
         notifyButton: {
